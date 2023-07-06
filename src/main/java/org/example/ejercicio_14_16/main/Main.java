9e9671852159901031c0eee44ab48c14cc348688
borja <borja@example.com>
2023-07-06 11:11:36 +0200

    pendiente limitar 1 hijo por pareja

--- src/main/java/org/example/ejercicio_14_16/main/Main.java
+++ src/main/java/org/example/ejercicio_14_16/main/Main.java
@@ -18,28 +18,35 @@ public class Main {
         Perro perro1 = new Perro("bulldog", "Chucho", "Joaquín", Sexo.MACHO, null, null, 0);
         Perro perro2 = new Perro("bulldog", "Chucha", "Felipe", Sexo.HEMBRA, null, null, 0);
         Perro perro3 = new Perro("bulldog", "Chuchon", "Carlos", Sexo.MACHO, null, null, 0);
-        Perro perro4 = new Perro("bulldog", "Chuchona", "Carlos", Sexo.HEMBRA, null, null, 0);
+        Perro perro4 = new Perro("bulldog", "Chuchona", "Ana", Sexo.HEMBRA, null, null, 0);
+        /*Perro perro5 = new Perro("bulldog", "Perraco", "Carlos", Sexo.MACHO, null, null, 0);
+        Perro perro6 = new Perro("bulldog", "Perraca", "Carlos", Sexo.HEMBRA, null, null, 0);*/
 
-        Gato gato1 = new Gato("angora", "Micho", "Andrea",Sexo.MACHO, null, null, 0);
-        Gato gato2 = new Gato("angora", "Micha", "Susana",Sexo.HEMBRA, null, null, 0);
+        Gato gato1 = new Gato("angora", "Micho", "Andrea", Sexo.MACHO, null, null, 0);
+        Gato gato2 = new Gato("angora", "Micha", "Susana", Sexo.HEMBRA, null, null, 0);
+        Gato gato3 = new Gato("persa", "Michinho", "Andrea", Sexo.MACHO, null, null, 0);
+        Gato gato4 = new Gato("persa", "Michinha", "Susana", Sexo.HEMBRA, null, null, 0);
 
-        Tucan tucan1 = new Tucan("comun",Sexo.MACHO, null, null, 0);
-        Tucan tucan2 = new Tucan("comun",Sexo.HEMBRA, null, null, 0);
+        Tucan tucan1 = new Tucan("comun", Sexo.MACHO, null, null, 0);
+        Tucan tucan2 = new Tucan("comun", Sexo.HEMBRA, null, null, 0);
 
-        PezPayaso pezPayaso1 = new PezPayaso("comun",Sexo.MACHO, null, null, 0);
-        PezPayaso pezPayaso2 = new PezPayaso("comun",Sexo.HEMBRA, null, null, 0);
+        PezPayaso pezPayaso1 = new PezPayaso("comun", Sexo.MACHO, null, null, 0);
+        PezPayaso pezPayaso2 = new PezPayaso("comun", Sexo.HEMBRA, null, null, 0);
 
-        Oveja oveja1 = new Oveja("churra", "Serafín",Sexo.MACHO, null, null, 0);
-        Oveja oveja2 = new Oveja("churra", "Serafín",Sexo.HEMBRA, null, null, 0);
+        Oveja oveja1 = new Oveja("churra", "Serafín", Sexo.MACHO, null, null, 0);
+        Oveja oveja2 = new Oveja("churra", "Serafín", Sexo.HEMBRA, null, null, 0);
 
-        Vaca vaca1 = new Vaca("rubia", "Laureano",Sexo.MACHO, null, null, 0);
-        Vaca vaca2 = new Vaca("rubia", "Laureano",Sexo.HEMBRA, null, null, 0);
+        Vaca vaca1 = new Vaca("rubia", "Laureano", Sexo.MACHO, null, null, 0);
+        Vaca vaca2 = new Vaca("rubia", "Laureano", Sexo.HEMBRA, null, null, 0);
         listaAnimales.add(perro1);
         listaAnimales.add(perro2);
         listaAnimales.add(perro3);
+        listaAnimales.add(perro4);
 
         listaAnimales.add(gato1);
         listaAnimales.add(gato2);
+        listaAnimales.add(gato3);
+        listaAnimales.add(gato4);
 
         listaAnimales.add(tucan1);
         listaAnimales.add(tucan2);
@@ -98,15 +105,16 @@ public class Main {
 
                     //en el siguiente orden
                     //si los candidatos son de la misma especie y distinto sexo
-                    //y si las madres y los padres de los candidatos son distintos o si son de la primera generación
+                    //y si las madres y los padres de los candidatos son distintos o si son de la primera generación(porque sus padres serin iguales a null)
                     //y no son padre e hijo
                     //y nos son madre e hijo
+                    //falta añadir la condicion de un hijo por pareja podemos probar con un arrayList de Hijos???
                     if (
                             ((listaAnimales.get(i)).getClass() == listaAnimales.get(j).getClass()) && (listaAnimales.get(i).getSexo() != listaAnimales.get(j).getSexo())
                                     && ((listaAnimales.get(i).getPadre() != listaAnimales.get(j).getPadre() && listaAnimales.get(i).getMadre() != listaAnimales.get(j).getMadre()) || listaAnimales.get(i).getGeneracion() == 0)
                                     && (listaAnimales.get(i).getPadre() != listaAnimales.get(j) || listaAnimales.get(j).getPadre() != listaAnimales.get(i))
                                     && (listaAnimales.get(i).getMadre() != listaAnimales.get(j) || listaAnimales.get(j).getMadre() != listaAnimales.get(i))
-                    ){
+                    ) {
                         //System.out.println("animal suitable para procrear" + listaAnimales.get(i).toString() + " con " + listaAnimales.get(j).toString());
 
                         //discernir si madre/padre es la j o la i, es decir, si la hembra/macho es el animal del for i o del j m es la madre y p es el padre
@@ -118,11 +126,11 @@ public class Main {
                             p = i;
                         }
                         if (listaAnimales.get(i).getTipo().equals(Tipo.PERRO)) {
-
                             listaAnimalesCria.add(new Perro("", "", "", Sexo.MACHO, listaAnimales.get(p), listaAnimales.get(m), generacion));//un perro macho tendra un hijo con cada perra(2)
-
+                            listaAnimalesCria.add(new Perro("", "", "", Sexo.HEMBRA, listaAnimales.get(p), listaAnimales.get(m), generacion));
                         } else if (listaAnimales.get(i).getTipo().equals(Tipo.GATO)) {
-                            listaAnimalesCria.add(new Gato("", "", "", null, listaAnimales.get(p), listaAnimales.get(m), generacion));
+                            listaAnimalesCria.add(new Gato("", "", "", Sexo.MACHO, listaAnimales.get(p), listaAnimales.get(m), generacion));
+                            listaAnimalesCria.add(new Gato("", "", "", Sexo.MACHO, listaAnimales.get(p), listaAnimales.get(m), generacion));
                         } else if (listaAnimales.get(i).getTipo().equals(Tipo.TUCAN)) {
                             listaAnimalesCria.add(new Tucan());
                         } else if (listaAnimales.get(i).getTipo().equals(Tipo.PEZ_PAYASO)) {
@@ -143,12 +151,16 @@ public class Main {
             for (Animal e : listaAnimalesCria) {//añado las crias a la listaAnimales
                 listaAnimales.add(e);
             }
-            for (Animal e : listaAnimalesCria) {
+            /*for (Animal e : listaAnimalesCria) {
                 System.out.println(e.toString());
-            }
+            }*/
 
             generacion++;
         } while (generacion < 4);/*"mientras el numero de animales sea distinto al bucle anterior generacion<4*/
+
+        for (Animal e : listaAnimales) {
+            System.out.println(e.toString());
+        }
         System.out.println(listaAnimales.size());
 
     }
